dd1937911456a2816fca9a9cb5a37e831d0e1c2a
Harshit <hars1614@gmail.com>
2020-03-08 11:53:01 +0530

    just basic rendering working currently; dummy code used in generate_sudoku function

---
+++ main.js
@@ -0,0 +1,40 @@
+const sudokus = [];
+
+function generate_sudoku(gaps_seed)
+{
+    const sudoku = [
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        []
+    ]
+
+    // dummy code to test the render
+    let nums = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
+
+    for (let i = 0; i < 81; i++){
+        sudoku[Math.floor(i/9)][i%9] = nums[i];
+    }
+
+    return sudoku;
+}
+
+let sudoku = generate_sudoku();
+
+let canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext("2d");
+let width = canvas.width;
+let height = canvas.height;
+
+(function setup()
+{
+    render = new Render(100, height-100, ctx, sudoku, 10)
+    render.renderBox();
+    render.renderValues();
+
+})();
---
+++ render.js
@@ -0,0 +1,82 @@
+// TODO ::
+/*  implement the FONTSIZE functionality
+    figure out a better way to add padding to the boxes
+*/
+class Render
+{
+    constructor(s, e, ctx, sudokuMap)
+    {
+        this.sudokuMap = sudokuMap;
+        this.unit = (e-s)/9;
+        this.x = s, this.y = s;
+        this.endingX = e, this.endingY = e;
+        this.ctx = ctx;
+        this.digitPadding = this.unit/2;
+        this.fontSize;
+    }
+
+    renderBox = function() {
+        // draw the sudoku box
+        let x = this.x,
+            y = this.y;
+
+        for (let i = 0; i < 10; i++)
+        {
+            console.log("weird behaviour", y, this.ctx.lineWidth, this.ctx.lineCap)
+            // paths
+            this.ctx.beginPath();
+            this.ctx.moveTo(x,y);
+            this.ctx.lineTo(this.x + this.unit * 9, y);
+            this.ctx.stroke();
+            this.ctx.closePath();
+            y += this.unit;
+        }
+
+        x = this.x,
+        y = this.y;
+        for (let i = 0; i < 10; i++)
+        {
+            console.log("weird behaviour", y, this.ctx.lineWidth, this.ctx.lineCap)
+            // paths
+            this.ctx.beginPath();
+            this.ctx.moveTo(x,y);
+            this.ctx.lineTo(x, this.y + this.unit * 9);
+            this.ctx.stroke();
+            this.ctx.closePath();
+            x += this.unit;
+        }
+    }
+
+    renderValues = function()
+    {
+        // code to display the sudoku
+        for (let i = 0; i < this.sudokuMap.length; i++) 
+        {
+            for (let j = 0; j < this.sudokuMap[i].length; j++) 
+            {
+                const elm = this.sudokuMap[i][j];
+                this.renderValueInPlace(elm, 
+                    {
+                        x:j, 
+                        y:i
+                    });
+            }
+        }
+    }
+
+    renderValueInPlace = function(val, { x:x, y:y }, edit=false) 
+    {
+        val = (val == 0) ? "x" : val;
+
+        x = this.x + this.unit * x;
+        y = this.y + this.unit * y;
+        
+        (edit == true) ? this.ctx.clearRect(x + this.ctx.lineWidth, y + this.ctx.lineWidth, this.unit - this.ctx.lineWidth, this.unit - this.ctx.lineWidth) : null;
+        
+        x += this.digitPadding - this.digitPadding/3;
+        y += this.digitPadding + this.digitPadding/4 + this.digitPadding/12;
+
+        this.ctx.font = "20px sans-serif";
+        this.ctx.fillText(val, x, y);
+    }
+}
