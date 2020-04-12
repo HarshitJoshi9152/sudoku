9638e80e9665cfda67f1ea8f4196445a6dbd3b4b
Harshit <hars1614@gmail.com>
2020-04-13 01:33:17 +0530

    fixed the coordinates function and successfully generated the box values coordinates

---
+++ getRangeCoors.js
@@ -0,0 +1,53 @@
+function getRangeCoors(s, e, dbg=false){
+    // s and e are arrays with std location formats in which [0] represents the x pos and [1] represents the y pos
+    const startValue = s[0] * s[1];
+    // const endValue =   e[0] * e[1];
+    const noOfCorsRequired = (e[0] + 1 - s[0]) * (e[1] + 1 - s[1])
+
+    // +1 because 9 - 7 = 2 but there are 3 position ie [7,8,9]
+    const verticalLimit = e[1] - s[1] + 1; // we dont actually need the verticalLimit var because we are filling in a horizontal fashion
+    const HorizontalLimit = e[0] - s[0] + 1;
+    
+    const coordinatesRange = [];
+    let yval = s[1];
+
+    if(dbg)
+        debugger;
+
+    for (let i = startValue; i < startValue + noOfCorsRequired; i++){
+        let xval = s[0] + i % HorizontalLimit;
+        
+        if( i != startValue && coordinatesRange.length % HorizontalLimit == 0){ // because 0 % anything == 0 : true
+            // we increase the y value when we overflow the HorizontalLimit and the x value when we overflow the VerticalLimit
+            // we didnt realise it till now because we were using ranges where hv and vv were the same
+            // but we dont have to watch for the verticalOverflow because we fill coors in a horizontal fashion
+            yval += 1;
+        }
+        // console.log(coordinatesRange.length);
+        let corrs = [xval, yval];
+        coordinatesRange.push(corrs)
+    }
+    // console.table(coordinatesRange);
+    return coordinatesRange;
+}
+
+getRangeCoors.__test = (dbg=false) => {
+
+    const experimentalValues = [
+        [[6,0],[8,2],"sign"],
+        [[7,2],[8,8],"!"],
+        [[2,7],[6,8],"&"],
+        [[0,6],[2,8],"E"]
+    ];
+    
+    for (let i of experimentalValues) {
+        console.table("testing for :", ...i);
+        for (let j of getRangeCoors(i[0], i[1], dbg)) {
+            // console.log(i[1], i[0],"hello");
+            render.renderValueInPlace(i[2] || "*", {
+                x: j[0],
+                y: j[1] // matrixRangeValues returns it according to the coordinate sys
+            }, true);
+        }
+    }
+}
--- solver.js
+++ solver.js
@@ -1,5 +1,6 @@
-// todo edit renderer.render to show coordinates to easily
-// write the indexes for the box values
+// todo 
+// edit renderer.render to show coordinates to easily DONE
+// generate the indexes of box values programmatically. 
 
 class Solver
 {
@@ -29,17 +30,9 @@ class Solver
             [],
             []
         ];
-        this.boxesCordinates = [
-            [0,0],[2,2],
-            // [3,3],[,2]
-        ];
 
         this.missingCoordinates = [];
-    }
 
-    solve = function ()
-    {
-        // code
     }
 
     analyse = function()
@@ -48,41 +41,86 @@ class Solver
         {
             for (let j in this.sudoku[i])
             {
-                let val = this.sudoku[i][j];
+                const val = this.sudoku[i][j];
                 if(val == 0)
                 {
-                    this.missingCoordinates.push([i,j]);
+                    this.missingCoordinates.push([j,i]);
+                    // because the followed fashion is of x-pos and then y-pos.
                 }
             }
         }
     }
 
-    SetSudoku = function (sudoku)
-    {
+    getValue = (pos) => {
+        return this.sudoku[pos[1]][pos[0]];
+        // because in 2d array first y-pos is given then x-pos is given
+    }
+
+    SetSudoku = (sudoku) => {
+
         this.sudoku = sudoku;
         this.rows = sudoku;
+        
         sudoku = sudoku.toString().replace(/,/g,"")
         for (let i in sudoku)
         {
             let val = sudoku[i];
             this.cols[i % 9].push(val);
         }
-        // for (let i=0;i<9;i += 2)
-        // {
-        //     this.boxes[0].push(
-        //         matrixRangeValues(this.boxesCordinates[i],this.boxesCordinates[i+1],this.sudoku)
-        //         );
-        // }
-        // for (let i in this.sudoku)
-        // {
-        //     for (let j in this.sudoku[i])
-        //     {
-        //         switch([i,j])
-        //         {
-        //             case []:
-        //         }
-        //     }
-        // }
+
+        let acc = 0;
+        for (let i of this.genBoxCoors()){
+            let coors = getRangeCoors(i[0],i[1]);
+            /* if you want to get coordinates only use : this.boxes[acc].push(this.sudoku[coors[1]][coors[0]]);*/
+            for (let j of coors) {
+                this.boxes[acc].push(this.sudoku[j[1]][j[0]]);
+            }
+            // because in arrays we first have to mention y-val then the x-val;
+            acc++;
+        }
+    }
+
+    genBoxCoors = () => {
+        // this first generates the x and y values of starting cube and then the x and y values of the corresponding cube by adding 2 to both x and y values
+        // i got the idea for this function by observing the pattern between the starting and ending coordinates.
+        const coors = [];
+        // this loop populates the coors array in a vertical fashion meaning first all the possible vertical y pos value combination with the identified x value are generated and then we move on to the next x value.
+        for (let i = 0; i < 9; i++){
+            if (i % 3 == 0){
+                for (let j = 0; j < 9; j++) {
+                    if (j % 3 == 0){
+                        coors.push([
+                            [i,j],
+                            [i + 2, j + 2]
+                        ]);
+                    }
+                }
+            }
+        }
+        return coors;
+    }
+
+    solve = () => {
+        const vals = [1,2,3,4,5,6,7,8,9];
+
+        // console.log(this.rows);
+        // console.log(this.cols);
+        // console.log(this.boxes);
+
+        for (let i in this.missingCoordinates){
+            console.log(this.missingCoordinates[i]);
+        }
+    }
+
+    // testingPurposes
+    __verifyMissingValueDiscrepancies = () => {
+        const missingValueObj = {};
+        for (let i of this.missingCoordinates){
+            if(this.getValue([i[0], i[1]]) != 0){
+                missingValueObj[i.toString()] = this.getValue([i[0], i[1]]);
+            }
+        }
+        return Object.keys(missingValueObj).length > 0 ? missingValueObj : null;
     }
 }
 
